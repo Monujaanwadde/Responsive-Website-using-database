31e28bbe3a3b6f92d52c4330170f263ce2310c8c
Monujaanwadde <125716579+Monujaanwadde@users.noreply.github.com>
2025-11-30 12:57:19 +0530

    Update email credentials in resend_otp.php

--- php/resend_otp.php
+++ php/resend_otp.php
@@ -26,12 +26,12 @@
         $mail->isSMTP();
         $mail->Host = "smtp.gmail.com";
         $mail->SMTPAuth = true;
-        $mail->Username = "godjanuwadde50@gmail.com";
-        $mail->Password = "tieq jnkc xsww vplm";
+        $mail->Username = "your_email_address";
+        $mail->Password = "email_app_password";
         $mail->SMTPSecure = "tls";
         $mail->Port = 587;
 
-        $mail->setFrom("godjanuwadde50@gmail.com", "OTP Verification");
+        $mail->setFrom("your_email_address", "OTP Verification");
         $mail->addAddress($_SESSION['user_data']['email']);
 
         $mail->isHTML(true);
